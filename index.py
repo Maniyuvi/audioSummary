767ba33464c5e98db043a4775aa7dab43634fbc1
Manikandan P <manikandanp@softsquare.biz>
2023-12-12 13:26:33 +0530

    added index file

--- index.py
+++ index.py
@@ -1,48 +1,69 @@
-# from flask import Flask, request
-# import streamlit as st
+# # from flask import Flask, request
+# # import streamlit as st
+# # import threading
+
+# # app = Flask(__name__)
+
+# # @app.route('/getdata/<url>', methods=['POST'])
+# # def get_data(url):
+# #     print('URL::::', url)
+# #     # Implement your logic to return data
+# #     # For example, fetching some data and returning it as JSON
+# #     data = {"key": "value"}  # Replace with your data fetching logic
+# #     st.write('API hits')
+# #     return data
+
+# # def run_flask():
+# #     app.run(port=5000)  # Run on a different port than Streamlit
+
+# # # Run Flask in a separate thread
+# # threading.Thread(target=run_flask).start()
+
+# # # Streamlit app code here
+# # st.title('My Streamlit App')
+# # # ...
+
+
+# from flask import Flask, request, jsonify
 # import threading
 
 # app = Flask(__name__)
 
-# @app.route('/getdata/<url>', methods=['POST'])
-# def get_data(url):
-#     print('URL::::', url)
-#     # Implement your logic to return data
-#     # For example, fetching some data and returning it as JSON
-#     data = {"key": "value"}  # Replace with your data fetching logic
-#     st.write('API hits')
-#     return data
+# @app.route('/api/getdata', methods=['GET'])
+# def get_data():
+#     # Your logic to fetch and return data
+#     data = {"message": "Hello from Streamlit + Flask!"}
+#     return jsonify(data)
+
+# @app.route('/api/postdata', methods=['POST'])
+# def post_data():
+#     # Example of handling POST data
+#     received_data = request.json
+#     return jsonify({"received": received_data})
 
 # def run_flask():
-#     app.run(port=5000)  # Run on a different port than Streamlit
+#     app.run(port=5000)
 
 # # Run Flask in a separate thread
 # threading.Thread(target=run_flask).start()
 
-# # Streamlit app code here
-# st.title('My Streamlit App')
-# # ...
 
+import streamlit as st
+import pandas as pd
 
-from flask import Flask, request, jsonify
-import threading
+# Create a DataFrame with some data
+data = {'Name': ['John', 'Jane', 'Bob'], 'Age': [25, 30, 35]}
+df = pd.DataFrame(data)
 
-app = Flask(__name__)
-
-@app.route('/api/getdata', methods=['GET'])
+# Create a Streamlit app that returns the data as JSON
+@st.cache
 def get_data():
-    # Your logic to fetch and return data
-    data = {"message": "Hello from Streamlit + Flask!"}
-    return jsonify(data)
+    return df.to_json()
 
-@app.route('/api/postdata', methods=['POST'])
-def post_data():
-    # Example of handling POST data
-    received_data = request.json
-    return jsonify({"received": received_data})
+@st.route('/data')
+def data():
+    return get_data()
 
-def run_flask():
-    app.run(port=5000)
+if __name__ == '__main__':
+    st.run_app()
 
-# Run Flask in a separate thread
-threading.Thread(target=run_flask).start()
