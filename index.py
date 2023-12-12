5b685ee6f4fee26081c81ad3726cd3b482220d90
Manikandan P <manikandanp@softsquare.biz>
2023-12-12 12:56:56 +0530

    added index file

--- index.py
+++ index.py
@@ -10,10 +10,11 @@ def get_data(url):
     # Implement your logic to return data
     # For example, fetching some data and returning it as JSON
     data = {"key": "value"}  # Replace with your data fetching logic
+    st.write('API hits')
     return data
 
 def run_flask():
-    app.run(port=3000)  # Run on a different port than Streamlit
+    app.run(port=5000)  # Run on a different port than Streamlit
 
 # Run Flask in a separate thread
 threading.Thread(target=run_flask).start()
